08d44d692f5714afe5083e82c43b04aaa7223561
Sreeram3927 <sreeram292004@gmail.com>
2023-03-11 12:26:19 +0530

    create node

--- ros2_ws/src/serial_chat/serial_chat/chat_node.py
+++ ros2_ws/src/serial_chat/serial_chat/chat_node.py
@@ -3,3 +3,10 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import serial
 import threading
+
+class SerialChatNode(Node):
+    def __init__(self):
+        super().__init__('serial_chat')
+
+        port = '/dev/ttyUSB0'
+        baudrate = 115200
