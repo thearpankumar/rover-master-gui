8a87a24bf007c6a7fdd2af65951cc2ac0dbfb7b4
Sreeram3927 <sreeram292004@gmail.com>
2023-03-11 12:32:36 +0530

    define callback

--- ros2_ws/src/serial_chat/serial_chat/chat_node.py
+++ ros2_ws/src/serial_chat/serial_chat/chat_node.py
@@ -14,3 +14,13 @@ class SerialChatNode(Node):
         self.serial = serial.Serial(port, baudrate, timeout=1)
         self.sub = self.create_subscription(String, 'serial_chat', self.callback, 10)
         self.pub = self.create_publisher(String, 'serial_chat', 10)
+        
+        self.serial_thread = threading.Thread(target=self.serial_callback)
+        self.serial_thread.start()
+
+        self.get_logger().info('SerialChatNode initialized')
+        
+    def callback(self, msg):
+        data = msg.data
+        self.get_logger().info('Received: %s' % data)
+        self.serial.write((data + '\n').encode('utf-8'))
