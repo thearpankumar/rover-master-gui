b5f5ad4582b428a7c62b86015e2aed2d3776a5e1
Sreeram3927 <sreeram292004@gmail.com>
2023-03-12 16:43:02 +0530

    create node

--- ros2_ws/src/network_chat/network_chat/send_data.py
+++ ros2_ws/src/network_chat/network_chat/send_data.py
@@ -1,2 +1,8 @@
 import rclpy
 from std_msgs.msg import String
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = rclpy.create_node('commander')
+    publisher = node.create_publisher(String, 'commands', 10)
