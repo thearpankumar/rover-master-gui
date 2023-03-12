c7bb3cf6ed8da086a05e1d561a49abbe383ded74
Sreeram3927 <sreeram292004@gmail.com>
2023-03-12 17:14:29 +0530

    call main()

--- ros2_ws/src/slave/slave/get_data.py
+++ ros2_ws/src/slave/slave/get_data.py
@@ -7,4 +7,12 @@ def main(args=None):
     node = rclpy.create_node('get_commands')
     subscription = node.create_subscription(String, 'commands', message_callback, 10)
     
-    
+    while rclpy.ok():
+        rclpy.spin_once(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
