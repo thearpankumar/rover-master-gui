972560aa951f526ed1e8938f75acf947242ee9ef
Sreeram3927 <sreeram292004@gmail.com>
2023-03-12 17:14:03 +0530

    create get_commands node

--- ros2_ws/src/slave/slave/get_data.py
+++ ros2_ws/src/slave/slave/get_data.py
@@ -1,2 +1,10 @@
 import rclpy
 from std_msgs.msg import String
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = rclpy.create_node('get_commands')
+    subscription = node.create_subscription(String, 'commands', message_callback, 10)
+    
+    
