8d2dd03dcf332342e3473bda7cd863037f545391
Sreeram3927 <sreeram292004@gmail.com>
2023-03-12 22:52:58 +0530

    call the main fn

--- ros2_ws/src/serial_chat/serial_chat/chat_screen.py
+++ ros2_ws/src/serial_chat/serial_chat/chat_screen.py
@@ -13,3 +13,12 @@ def main(args=None) :
     node = rclpy.create_node('chat_screen')
     received_messages = node.create_subscription(String, 'received_msgs', received_callback, 10)
     sent_messages = node.create_subscription(String, 'sent_msgs', sent_callback, 10)
+    
+    while rclpy.ok():
+        rclpy.spin_once(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
