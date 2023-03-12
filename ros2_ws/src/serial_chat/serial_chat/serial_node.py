1beec799228425df3948264991bfeafa1cd6026e
Sreeram3927 <sreeram292004@gmail.com>
2023-03-12 23:31:47 +0530

    change SerialChatNode to SerialNode

--- ros2_ws/src/serial_chat/serial_chat/serial_node.py
+++ ros2_ws/src/serial_chat/serial_chat/serial_node.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 import serial
 #import threading
 
-class SerialChatNode(Node):
+class SerialNode(Node):
     def __init__(self):
         super().__init__('serial_data')
 
@@ -19,7 +19,7 @@ class SerialChatNode(Node):
         # self.serial_thread = threading.Thread(target=self.serial_callback)
         # self.serial_thread.start()
 
-        self.get_logger().info('SerialChatNode initialized')
+        self.get_logger().info('SerialNode initialized')
         
     def callback(self):
         received_data = serial.readline().decode().strip()
@@ -30,19 +30,21 @@ class SerialChatNode(Node):
     #     self.get_logger().info('Received: %s' % data)
     #     self.serial.write((data + '\n').encode('utf-8'))
       
-    def serial_callback(self):
-        while rclpy.ok():
-            if self.serial.in_waiting > 0:
-                data = self.serial.readline().decode('utf-8').strip()
-                self.get_logger().info('Received: %s' % data)
-                msg = String()
-                msg.data = data
-                self.pub.publish(msg)
+    # def serial_callback(self):
+    #     while rclpy.ok():
+    #         if self.serial.in_waiting > 0:
+    #             data = self.serial.readline().decode('utf-8').strip()
+    #             self.get_logger().info('Received: %s' % data)
+    #             msg = String()
+    #             msg.data = data
+    #             self.pub.publish(msg)
+    
+
                 
 def main(args=None):
     rclpy.init(args=args)
     
-    node = SerialChatNode()
+    node = SerialNode()
     rclpy.spin(node)
     # node.serial_thread.join()
     
