665f4813464f48fb05ab3561ee446ad7b32e1012
Sreeram3927 <sreeram292004@gmail.com>
2023-03-14 11:22:22 +0530

    def stop and forward

--- ros2_ws/src/slave/slave/get_data.py
+++ ros2_ws/src/slave/slave/get_data.py
@@ -13,15 +13,22 @@ class Rover(Node):
         port2 = '/dev/ttyUSB1'
         baudrate2 = 9600
         
-        sabertooth1 = serial.Serial(port=port1, baudrate=baudrate1)
-        sabertooth2 = serial.Serial(port=port2, baudrate=baudrate2)
+        saber1 = serial.Serial(port=port1, baudrate=baudrate1)
+        saber2 = serial.Serial(port=port2, baudrate=baudrate2)
+        
+        saber1Motor1 = 64
+        saber1Motor2 = 192
+        saber2Motor1 = 64
+        saber2Motor2 = 192
+        
+        stop()
         
         self.subscription = self.create_subscription(String, 'commands', message_callback, 10)
 
-        def message_callback(msg):
+        def message_callback(self, msg):
             #print('Received message: "%s"' % msg.data)
             cmd(msg)
-            
+        
         def cmd(data):
             if data == "forward":
                 print("forward")
@@ -33,6 +40,40 @@ class Rover(Node):
                 print("right")
             else:
                 print("invalid")
+                
+        def forward():
+            
+            saber1Motor1 += 5
+            if saber1Motor1 > 127:
+                saber1Motor1 = 127
+            output = bytes([saber1Motor1])
+            saber1.write(output)
+            print("saber1Motor1 speed = ", saber1Motor1, " = ", output)
+            
+            saber1Motor2 += 5
+            if saber1Motor2 > 255:
+                saber1Motor2 = 255
+            output = bytes([saber1Motor2])
+            saber1.write(output)
+            print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
+            
+            saber2Motor1 += 5
+            if saber2Motor1 > 127:
+                saber2Motor1 = 127
+            output = bytes([saber2Motor1])
+            saber2.write(output)
+            print("saber2Motor1 speed = ", saber2Motor1, " = ", output)
+            
+            saber2Motor2 += 5
+            if saber2Motor2 > 255:
+                saber2Motor2 = 255
+            output = bytes([saber2Motor2])
+            saber2.write(output)
+            print("saber2Motor2peed = ", saber2Motor2, " = ", output)
+                
+        def stop():
+            saber1.write(bytes([0]))
+            saber2.write(bytes([0]))
                 
 def main(args=None):
     rclpy.init(args=args)
