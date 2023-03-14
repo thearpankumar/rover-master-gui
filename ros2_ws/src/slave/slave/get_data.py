3dd25935f972b77c75662e27068bb4285060468c
Sreeram3927 <sreeram292004@gmail.com>
2023-03-14 10:51:10 +0530

    create motor command fn

--- ros2_ws/src/slave/slave/get_data.py
+++ ros2_ws/src/slave/slave/get_data.py
@@ -1,18 +1,33 @@
 import rclpy
 from std_msgs.msg import String
+from rclpy.node import Node
 
-def message_callback(msg):
-    print('Received message: "%s"' % msg.data)
+class Rover(Node):
+    
+    def __init__(self):
+        super.__init__('rover_code')
+        self.subscription = self.create_subscription(String, 'commands', message_callback, 10)
+
+        def message_callback(msg):
+            #print('Received message: "%s"' % msg.data)
+            cmd()
+            
+        def cmd(data):
+            if data == "forward":
+                print("forward")
+            elif data == "backard":
+                print("backard")
+            elif data == "left":
+                print("left")
+            elif data == "right":
+                print("right")
 
 def main(args=None):
     rclpy.init(args=args)
 
-    node = rclpy.create_node('get_commands')
-    subscription = node.create_subscription(String, 'commands', message_callback, 10)
+    node = Rover()
+    # subscription = node.create_subscription(String, 'commands', message_callback, 10)
     
-    while rclpy.ok():
-        rclpy.spin_once(node)
-
     node.destroy_node()
     rclpy.shutdown()
 
