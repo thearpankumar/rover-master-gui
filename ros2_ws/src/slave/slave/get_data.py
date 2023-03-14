fcdb401e355813ab1adde48fee8daf3655ce4390
Sreeram3927 <sreeram292004@gmail.com>
2023-03-14 16:24:56 +0530

    get user input

--- ros2_ws/src/slave/slave/get_data.py
+++ ros2_ws/src/slave/slave/get_data.py
@@ -13,163 +13,170 @@ class Rover(Node):
         port2 = '/dev/ttyUSB1'
         baudrate2 = 9600
         
-        saber1 = serial.Serial(port=port1, baudrate=baudrate1)
-        saber2 = serial.Serial(port=port2, baudrate=baudrate2)
+        self.saber1 = serial.Serial(port=port1, baudrate=baudrate1)
+        self.saber2 = serial.Serial(port=port2, baudrate=baudrate2)
         
-        saber1Motor1 = 64
-        saber1Motor2 = 192
-        saber2Motor1 = 64
-        saber2Motor2 = 192
+        self.saber1Motor1 = 64
+        self.saber1Motor2 = 192
+        self.saber2Motor1 = 64
+        self.saber2Motor2 = 192
         
-        stop()
+        self.stop()
         
-        self.subscription = self.create_subscription(String, 'commands', message_callback, 10)
+        self.subscription = self.create_subscription(String, 'commands', self.message_callback, 10)
 
-        def message_callback(self, msg):
-            #print('Received message: "%s"' % msg.data)
-            cmd(msg)
+    def message_callback(self, msg):
+        #print('Received message: "%s"' % msg.data)
+        self.cmd(msg)
         
-        def cmd(data):
-            if data == "forward":
-                print("forward")
-            elif data == "backard":
-                print("backard")
-            elif data == "left":
-                print("left")
-            elif data == "right":
-                print("right")
-            else:
-                print("invalid")
-                
-        def forward():
+    def user_input(self):
+        while rclpy.ok:
+            msg = input("Enter directions/stop: ")
+            self.cmd(msg)
+        self.stop()
+    
+    def cmd(self,data):
+        if data == "forward":
+            self.forward()
+        elif data == "backard":
+            self.backward()
+        elif data == "left":
+            self.left()
+        elif data == "right":
+            self.right()
+        else:
+            print("invalid")
             
-            saber1Motor1 += 5
-            if saber1Motor1 > 127:
-                saber1Motor1 = 127
-            output = bytes([saber1Motor1])
-            saber1.write(output)
-            print("saber1Motor1 speed = ", saber1Motor1, " = ", output)
-            
-            saber1Motor2 += 5
-            if saber1Motor2 > 255:
-                saber1Motor2 = 255
-            output = bytes([saber1Motor2])
-            saber1.write(output)
-            print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
-            
-            saber2Motor1 += 5
-            if saber2Motor1 > 127:
-                saber2Motor1 = 127
-            output = bytes([saber2Motor1])
-            saber2.write(output)
-            print("saber2Motor1 speed = ", saber2Motor1, " = ", output)
-            
-            saber2Motor2 += 5
-            if saber2Motor2 > 255:
-                saber2Motor2 = 255
-            output = bytes([saber2Motor2])
-            saber2.write(output)
-            print("saber2Motor2peed = ", saber2Motor2, " = ", output)
-            
-        def backward():
-            
-            saber1Motor1 -= 5
-            if saber1Motor1 < 1:
-                saber1Motor1 = 1
-            output = bytes([saber1Motor1])
-            saber1.write(output)
-            print("saber1Motor1 speed = ", saber1Motor1, " = ", output)
-            
-            saber1Motor2 -= 5
-            if saber1Motor2 < 128:
-                saber1Motor2 = 128
-            output = bytes([saber1Motor2])
-            saber1.write(output)
-            print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
-            
-            saber2Motor1 -= 5
-            if saber2Motor1 < 1:
-                saber2Motor1 = 1
-            output = bytes([saber2Motor1])
-            saber2.write(output)
-            print("saber2Motor1 speed = ", saber2Motor1, " = ", output)
-            
-            saber2Motor2 -= 5
-            if saber2Motor2 < 128:
-                saber2Motor2 = 128
-            output = bytes([saber2Motor2])
-            saber2.write(output)
-            print("saber2Motor2peed = ", saber2Motor2, " = ", output)
-            
-        def left():
-            
-            saber1Motor1 = 127
-            # if saber1Motor1 > 127:
-            #     saber1Motor1 = 127
-            output = bytes([saber1Motor1])
-            saber1.write(output)
-            print("saber1Motor1 speed = ", saber1Motor1, " = ", output)
-            
+    def forward(self):
+        
+        self.saber1Motor1 += 5
+        if self.saber1Motor1 > 127:
+            self.saber1Motor1 = 127
+        output = bytes([self.saber1Motor1])
+        self.saber1.write(output)
+        print("self.saber1Motor1 speed = ", self.saber1Motor1, " = ", output)
+        
+        saber1Motor2 += 5
+        if saber1Motor2 > 255:
             saber1Motor2 = 255
-            # if saber1Motor2 > 255:
-            #     saber1Motor2 = 255
-            output = bytes([saber1Motor2])
-            saber1.write(output)
-            print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
-            
-            saber2Motor1 = 1
-            # if saber2Motor1 > 127:
-            #     saber2Motor1 = 127
-            output = bytes([saber2Motor1])
-            saber2.write(output)
-            print("saber2Motor1 speed = ", saber2Motor1, " = ", output)
-            
-            saber2Motor2 = 128
-            # if saber2Motor2 > 255:
-            #     saber2Motor2 = 255
-            output = bytes([saber2Motor2])
-            saber2.write(output)
-            print("saber2Motor2peed = ", saber2Motor2, " = ", output)
-            
-        def left():
-            
-            saber1Motor1 = 1
-            # if saber1Motor1 > 127:
-            #     saber1Motor1 = 127
-            output = bytes([saber1Motor1])
-            saber1.write(output)
-            print("saber1Motor1 speed = ", saber1Motor1, " = ", output)
-            
+        output = bytes([saber1Motor2])
+        self.saber1.write(output)
+        print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
+        
+        self.saber2Motor1 += 5
+        if self.saber2Motor1 > 127:
+            self.saber2Motor1 = 127
+        output = bytes([self.saber2Motor1])
+        self.saber2.write(output)
+        print("self.saber2Motor1 speed = ", self.saber2Motor1, " = ", output)
+        
+        self.saber2Motor2 += 5
+        if self.saber2Motor2 > 255:
+            self.saber2Motor2 = 255
+        output = bytes([self.saber2Motor2])
+        self.saber2.write(output)
+        print("self.saber2Motor2peed = ", self.saber2Motor2, " = ", output)
+        
+    def backward(self):
+        
+        self.saber1Motor1 -= 5
+        if self.saber1Motor1 < 1:
+            self.saber1Motor1 = 1
+        output = bytes([self.saber1Motor1])
+        self.saber1.write(output)
+        print("self.saber1Motor1 speed = ", self.saber1Motor1, " = ", output)
+        
+        saber1Motor2 -= 5
+        if saber1Motor2 < 128:
             saber1Motor2 = 128
-            # if saber1Motor2 > 255:
-            #     saber1Motor2 = 255
-            output = bytes([saber1Motor2])
-            saber1.write(output)
-            print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
+        output = bytes([saber1Motor2])
+        self.saber1.write(output)
+        print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
+        
+        saber2Motor1 -= 5
+        if saber2Motor1 < 1:
+            saber2Motor1 = 1
+        output = bytes([saber2Motor1])
+        self.saber2.write(output)
+        print("saber2Motor1 speed = ", saber2Motor1, " = ", output)
+        
+        saber2Motor2 -= 5
+        if saber2Motor2 < 128:
+            saber2Motor2 = 128
+        output = bytes([saber2Motor2])
+        self.saber2.write(output)
+        print("saber2Motor2peed = ", saber2Motor2, " = ", output)
+        
+    def left(self):
+        
+        self.saber1Motor1 = 127
+        # if self.saber1Motor1 > 127:
+        #     self.saber1Motor1 = 127
+        output = bytes([self.saber1Motor1])
+        self.saber1.write(output)
+        print("self.saber1Motor1 speed = ", self.saber1Motor1, " = ", output)
+        
+        saber1Motor2 = 255
+        # if saber1Motor2 > 255:
+        #     saber1Motor2 = 255
+        output = bytes([saber1Motor2])
+        self.saber1.write(output)
+        print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
+        
+        saber2Motor1 = 1
+        # if saber2Motor1 > 127:
+        #     saber2Motor1 = 127
+        output = bytes([saber2Motor1])
+        self.saber2.write(output)
+        print("saber2Motor1 speed = ", saber2Motor1, " = ", output)
+        
+        saber2Motor2 = 128
+        # if saber2Motor2 > 255:
+        #     saber2Motor2 = 255
+        output = bytes([saber2Motor2])
+        self.saber2.write(output)
+        print("saber2Motor2peed = ", saber2Motor2, " = ", output)
+        
+    def right(self):
+        
+        self.saber1Motor1 = 1
+        # if self.saber1Motor1 > 127:
+        #     self.saber1Motor1 = 127
+        output = bytes([self.saber1Motor1])
+        self.saber1.write(output)
+        print("self.saber1Motor1 speed = ", self.saber1Motor1, " = ", output)
+        
+        saber1Motor2 = 128
+        # if saber1Motor2 > 255:
+        #     saber1Motor2 = 255
+        output = bytes([saber1Motor2])
+        self.saber1.write(output)
+        print("saber1Motor2 speed = ", saber1Motor2, " = ", output)
+        
+        saber2Motor1 = 127
+        # if saber2Motor1 > 127:
+        #     saber2Motor1 = 127
+        output = bytes([saber2Motor1])
+        self.saber2.write(output)
+        print("saber2Motor1 speed = ", saber2Motor1, " = ", output)
+        
+        saber2Motor2 = 255
+        # if saber2Motor2 > 255:
+        #     saber2Motor2 = 255
+        output = bytes([saber2Motor2])
+        self.saber2.write(output)
+        print("saber2Motor2peed = ", saber2Motor2, " = ", output)
             
-            saber2Motor1 = 127
-            # if saber2Motor1 > 127:
-            #     saber2Motor1 = 127
-            output = bytes([saber2Motor1])
-            saber2.write(output)
-            print("saber2Motor1 speed = ", saber2Motor1, " = ", output)
-            
-            saber2Motor2 = 255
-            # if saber2Motor2 > 255:
-            #     saber2Motor2 = 255
-            output = bytes([saber2Motor2])
-            saber2.write(output)
-            print("saber2Motor2peed = ", saber2Motor2, " = ", output)
-                
-        def stop():
-            saber1.write(bytes([0]))
-            saber2.write(bytes([0]))
+    def stop(self):
+        self.saber1.write(bytes([0]))
+        self.saber2.write(bytes([0]))
                 
 def main(args=None):
     rclpy.init(args=args)
 
     node = Rover()
     # subscription = node.create_subscription(String, 'commands', message_callback, 10)
+    Rover.user_input()
     
     node.destroy_node()
     rclpy.shutdown()
