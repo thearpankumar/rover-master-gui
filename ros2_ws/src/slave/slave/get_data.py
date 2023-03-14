82c7951c8d1790570ab920c8fde35d4ce40b70e6
Sreeram3927 <sreeram292004@gmail.com>
2023-03-14 10:57:55 +0530

    create serial connection

--- ros2_ws/src/slave/slave/get_data.py
+++ ros2_ws/src/slave/slave/get_data.py
@@ -1,16 +1,26 @@
 import rclpy
 from std_msgs.msg import String
 from rclpy.node import Node
+import serial
 
 class Rover(Node):
     
     def __init__(self):
         super.__init__('rover_code')
+        
+        port1 = '/dev/ttyUSB0'
+        baudrate1 = 9600
+        port2 = '/dev/ttyUSB1'
+        baudrate2 = 9600
+        
+        sabertooth1 = serial.Serial(port=port1, baudrate=baudrate1)
+        sabertooth2 = serial.Serial(port=port2, baudrate=baudrate2)
+        
         self.subscription = self.create_subscription(String, 'commands', message_callback, 10)
 
         def message_callback(msg):
             #print('Received message: "%s"' % msg.data)
-            cmd()
+            cmd(msg)
             
         def cmd(data):
             if data == "forward":
@@ -21,7 +31,9 @@ class Rover(Node):
                 print("left")
             elif data == "right":
                 print("right")
-
+            else:
+                print("invalid")
+                
 def main(args=None):
     rclpy.init(args=args)
 
